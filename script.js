587b963942e1c43a871821873b57c90d2794b3b8
Mattia Vidoni <vidonimattia@gmail.com>
2022-07-08 23:08:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function generateQR(){
-    document.querySelector("#qr-image").style.display="block";
-    let QRtext=document.querySelector("#text").value;
+    document.getElementById("qr-image").style.display="block";
+    let QRtext=document.getElementById("text").value;
   
     if(QRtext.trim().length==0){
-        document.querySelector("#qr-image .error").innerHTML="Please enter text";
-        document.querySelector("#img").style.display="none";
+        document.getElementById("qr-image .error").innerHTML="Please enter text";
+        document.getElementById("img").style.display="none";
     }
     else{
-        document.querySelector("#img").style.display="block";
-        document.querySelector("#qr-image .error").innerHTML="";
-        document.querySelector("#img").src="https://api.qrserver.com/v1/create-qr-code/?size=240*240&data="+QRtext;
+        document.getElementById("img").style.display="block";
+        document.getElementById("qr-image .error").innerHTML="";
+        document.getElementById("img").src="https://api.qrserver.com/v1/create-qr-code/?size=240*240&data="+QRtext;
     }
 }
